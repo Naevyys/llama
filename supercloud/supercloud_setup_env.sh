44d0bf52b0dd3acfb7d5edc36e681b95c93c548a
Naevyys <siriremerk@gmail.com>
2024-02-16 17:57:48 -0500

    Updated supercloud setup, but likely not done

--- supercloud/supercloud_setup_env.sh
+++ supercloud/supercloud_setup_env.sh
@@ -1,19 +1,19 @@
-# Loading the required module
+#!/bin/bash
+
+# Loading the required modules
+source /etc/profile
+module load anaconda/2023a-pytorch
+module load nccl/2.18.1-cuda11.8  # Also loads module cuda/11.8 as it is a requirement
+
+# Installing packages needed
 mkdir /state/partition1/user/$USER
 export TMPDIR=/state/partition1/user/$USER
-source /etc/profile
-module load anaconda/2023a
+pip install --user --no-cache-dir -r ~/llama/requirements.txt  # Install requirements.txt
+pip3 install --user --no-cache-dir torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118  # Install pytorch with cuda 11.8. NOTE: When I run it after the rest it doesn't install anything new, maybe I don't need this command at all
+#pip install --user --no-cache-dir numpy pandas matplotlib seaborn ipykernel  # Check if I need any of that
 
-pip install --user --no-cache-dir transformers
-pip install --user --no-cache-dir bitsandbytes
-pip install --user --no-cache-dir torch datasets accelerate
-#pip install --user --no-cache-dir random
-pip install --user --no-cache-dir pandas
-pip install --user --no-cache-dir torch
-pip install --user --no-cache-dir huggingface_hub[cli]
-
-git config --global credential.helper store
-export HUGGINGFACE_TOKEN=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
-huggingface-cli login --token $HUGGINGFACE_TOKEN --add-to-git-credential
+#git config --global credential.helper store
+#export HUGGINGFACE_TOKEN=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
+#huggingface-cli login --token $HUGGINGFACE_TOKEN --add-to-git-credential
 
 #rm -rf /state/partition1/user/$USER
